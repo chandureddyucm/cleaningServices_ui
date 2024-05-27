5e9c6bfd06bc705dce3a810d7ba38b4d0f531740
Chandra <chandrakiranreddy10@gmail.com>
2024-05-27 15:32:10 -0500

    Added Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './user/login/login.component';
+import { UserRegisterComponent } from './user/register/register.component';
+import { StaffLoginComponent } from './staff/login/login.component';
+import { StaffRegisterComponent } from './staff/register/register.component';
+import { AdminLoginComponent } from './admin/login/login.component';
+import { AdminRegisterComponent } from './admin/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { UserHomeComponent } from './user/home/home.component';
+import { StaffHomeComponent } from './staff/home/home.component';
+import { AdminHomeComponent } from './admin/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+
+  { path: 'user-login', component: UserLoginComponent },
+  { path: 'user-register', component: UserRegisterComponent },
+  { path: 'user-home', component: UserHomeComponent },
+
+  { path: 'staff-login', component: StaffLoginComponent },
+  { path: 'staff-register', component: StaffRegisterComponent },
+  { path: 'staff-home', component: StaffHomeComponent },
+
+  { path: 'admin-login', component: AdminLoginComponent },
+  { path: 'admin-register', component: AdminRegisterComponent },
+  { path: 'admin-home', component: AdminHomeComponent },
+
+  //{ path: 'user/:id', component: UserHomeComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin/login/login.component';
+import { AdminRegisterComponent } from './admin/register/register.component';
+import { StaffLoginComponent } from './staff/login/login.component';
+import { StaffRegisterComponent } from './staff/register/register.component';
+import { UserLoginComponent } from './user/login/login.component';
+import { UserRegisterComponent } from './user/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AdminHomeComponent } from './admin/home/home.component';
+import { StaffHomeComponent } from './staff/home/home.component';
+import { UserHomeComponent } from './user/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    AdminRegisterComponent,
+    AdminHomeComponent,
+    StaffLoginComponent,
+    StaffRegisterComponent,
+    StaffHomeComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+    UserHomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
